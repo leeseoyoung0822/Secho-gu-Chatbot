027fca20a99e4f878324c6987a6c92888be2596e
이서영 <seoyoung@iseoyeong-ui-MacBookAir.local>
2024-12-14 17:05:11 +0900

    sidebar

---
+++ help.js
@@ -0,0 +1,11 @@
+// help.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    const helpButton = document.querySelector(".help-button");
+
+    if (helpButton) {
+        helpButton.addEventListener("click", function () {
+            window.location.href = "help.html"; // Help 버튼 클릭 시 help.html로 이동
+        });
+    }
+});
